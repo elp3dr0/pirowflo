ce88a6977b7fcd11e403cc315db44a3817668ff4
elp3dr0 <64701822+elp3dr0@users.noreply.github.com>
2025-04-15 10:45:33 +0100

    Adding debug logging to follow control flow through ble

--- src/adapters/ble/waterrowerble.py
+++ src/adapters/ble/waterrowerble.py
@@ -54,41 +54,49 @@ logger = logging.getLogger(__name__)
 mainloop = None
 
 class InvalidArgsException(dbus.exceptions.DBusException):
+    logger.debug("Entering class InvalidArgsException")
     _dbus_error_name = "org.freedesktop.DBus.Error.InvalidArgs"
 
 
 class NotSupportedException(dbus.exceptions.DBusException):
+    logger.debug("Entering class NotSupportedException")
     _dbus_error_name = "org.bluez.Error.NotSupported"
 
 
 class NotPermittedException(dbus.exceptions.DBusException):
+    logger.debug("Entering class NotPermittedException")
     _dbus_error_name = "org.bluez.Error.NotPermitted"
 
 
 class InvalidValueLengthException(dbus.exceptions.DBusException):
+    logger.debug("Entering class InvalidValueLengthException")
     _dbus_error_name = "org.bluez.Error.InvalidValueLength"
 
 
 class FailedException(dbus.exceptions.DBusException):
+    logger.debug("Entering class FailedException")
     _dbus_error_name = "org.bluez.Error.Failed"
 
 
 def register_app_cb():
-    logger.info("GATT application registered")
+    logger.debug("Entering register_app_cb")
+    #logger.info("GATT application registered")
 
 
 def register_app_error_cb(error):
-    logger.critical("Failed to register application: " + str(error))
+    logger.debug("Entering register_app_error_cb")
+    #logger.critical("Failed to register application: " + str(error))
     mainloop.quit()
 
 # Function is needed to trigger the reset of the waterrower. It puts the "reset_ble" into the queue (FIFO) in order
 # for the WaterrowerInterface thread to get the signal to reset the waterrower.
 
 def request_reset_ble():
+    logger.debug("Entering request_reset_ble")
     out_q_reset.put("reset_ble")
 
 def Convert_Waterrower_raw_to_byte():
-
+    logger.debug("Entering Conert_Waterrower_raw_to_byte")
     WRBytearray = []
     #print("Ble Values: {0}".format(WaterrowerValuesRaw))
     #todo refactor this part with the correct struct.pack e.g. 2 bytes use "H" instand of bitshifiting ?
@@ -115,6 +123,7 @@ def Convert_Waterrower_raw_to_byte():
 
 
 class DeviceInformation(Service):
+    logger.debug("Entering class DeviceInformation")
     DEVICE_INFORMATION_UUID = '180A'
 
     def __init__(self, bus, index):
@@ -128,9 +137,11 @@ class DeviceInformation(Service):
 
 
 class ManufacturerNameString(Characteristic):
+    logger.debug("Entering class ManuracturerNameString")
     MANUFACTURER_NAME_STRING_UUID = '2a29'
 
     def __init__(self, bus, index, service):
+        logger.debug("Entering ManufacturerNameString.init")
         Characteristic.__init__(
             self, bus, index,
             self.MANUFACTURER_NAME_STRING_UUID,
@@ -142,13 +153,16 @@ class ManufacturerNameString(Characteristic):
 
 
     def ReadValue(self, options):
+        logger.debug("Entering ManufacturerNameString.ReadValue")
         print('ManufacturerNameString: ' + repr(self.value))
         return self.value
 
 class ModelNumberString(Characteristic):
+    logger.debug("Entering class ModelNumberString")
     MODEL_NUMBER_STRING_UUID = '2a24'
 
     def __init__(self, bus, index, service):
+        logger.debug("Entering ModelNumberString.init")
         Characteristic.__init__(
             self, bus, index,
             self.MODEL_NUMBER_STRING_UUID,
@@ -160,13 +174,16 @@ class ModelNumberString(Characteristic):
 
 
     def ReadValue(self, options):
+        logger.debug("Entering ModelNumberString.ReadValue")
         print('ModelNumberString: ' + repr(self.value))
         return self.value
 
 class SerialNumberSring(Characteristic):
+    logger.debug("Entering Class SerialNumberString")
     MANUFACTURER_NAME_STRING_UUID = '2a25'
 
     def __init__(self, bus, index, service):
+        logger.debug("Entering SerialNumberString.init")
         Characteristic.__init__(
             self, bus, index,
             self.MANUFACTURER_NAME_STRING_UUID,
@@ -178,13 +195,16 @@ class SerialNumberSring(Characteristic):
 
 
     def ReadValue(self, options):
+        logger.debug("Entering SerialNumberString.ReadValue")
         print('SerialNumberSring: ' + repr(self.value))
         return self.value
 
 class HardwareRevisionString(Characteristic):
+    logger.debug("Entering Class HardwareRevisioString")
     MANUFACTURER_NAME_STRING_UUID = '2a27'
 
     def __init__(self, bus, index, service):
+        logger.debug("Entering HardwareRevisionString.init")
         Characteristic.__init__(
             self, bus, index,
             self.MANUFACTURER_NAME_STRING_UUID,
@@ -196,13 +216,16 @@ class HardwareRevisionString(Characteristic):
 
 
     def ReadValue(self, options):
+        logger.debug("Entering HardwareRevisionString.ReadValue")
         print('HardwareRevisionString: ' + repr(self.value))
         return self.value
 
 class FirmwareRevisionString(Characteristic):
+    logger.debug("Entering Class FirmwareRevisionString")
     MANUFACTURER_NAME_STRING_UUID = '2a26'
 
     def __init__(self, bus, index, service):
+        logger.debug("Entering FirmwareRevisionString.init")
         Characteristic.__init__(
             self, bus, index,
             self.MANUFACTURER_NAME_STRING_UUID,
@@ -214,13 +237,16 @@ class FirmwareRevisionString(Characteristic):
 
 
     def ReadValue(self, options):
+        logger.debug("Entering FirmwareRevisionString.ReadValue")
         print('FirmwareRevisionString: ' + repr(self.value))
         return self.value
 
 class SoftwareRevisionString(Characteristic):
+    logger.debug("Entering Class SoftwareRevisionsString")
     SOFTWARE_REVISION_STRING_UUID = '2a28'
 
     def __init__(self, bus, index, service):
+        logger.debug("Entering SoftwareRevisionsString.init")
         Characteristic.__init__(
             self, bus, index,
             self.SOFTWARE_REVISION_STRING_UUID,
@@ -236,13 +262,16 @@ class SoftwareRevisionString(Characteristic):
         #self.value[3] = 0x30
 
     def ReadValue(self, options):
+        logger.debug("Entering SoftwareRevisionsString.ReadValue")
         print('SoftwareRevisionString: ' + repr(self.value))
         return self.value
 
 class FTMservice(Service):
+    logger.debug("Entering Class FTMservice")
     FITNESS_MACHINE_UUID = '1826'
 
     def __init__(self, bus, index):
+        logger.debug("Entering FTMService.init")
         Service.__init__(self, bus, index, self.FITNESS_MACHINE_UUID, True)
         self.add_characteristic(FitnessMachineFeature(bus,0,self))
         self.add_characteristic(RowerData(bus, 1, self))
@@ -250,10 +279,12 @@ class FTMservice(Service):
 
 
 class FitnessMachineFeature(Characteristic):
+    logger.debug("Entering Class FitnessMachineFeature")
 
     FITNESS_MACHINE_FEATURE_UUID = '2acc'
 
     def __init__(self, bus, index, service):
+        logger.debug("Entering Class FitnessMachineFeature.init")
         Characteristic.__init__(
             self, bus, index,
             self.FITNESS_MACHINE_FEATURE_UUID,
@@ -277,14 +308,17 @@ class FitnessMachineFeature(Characteristic):
 
 
     def ReadValue(self, options):
+        logger.debug("Entering FitnessMachineFeature.ReadValue")
         print('Fitness Machine Feature: ' + repr(self.value))
         return self.value
 
 class RowerData(Characteristic):
+    logger.debug("Entering Class RowerData")
     ROWING_UUID = '2ad1'
     last_values = {}
 
     def __init__(self, bus, index, service):
+        logger.debug("Entering RowerData.init")
         Characteristic.__init__(
             self, bus, index,
             self.ROWING_UUID,
@@ -294,6 +328,7 @@ class RowerData(Characteristic):
         self.iter = 0
 
     def Waterrower_cb(self):
+        logger.debug("Entering RowerData.Waterrower_cb")
         Waterrower_byte_values = Convert_Waterrower_raw_to_byte()
         if self.last_values != Waterrower_byte_values:
             self.last_values = Waterrower_byte_values 
@@ -310,6 +345,7 @@ class RowerData(Characteristic):
         return self.notifying
 
     def _update_Waterrower_cb_value(self):
+        logger.debug("Entering RowerData.update_Waterrower_cb_value")
         print('Update Waterrower Rower Data')
 
         if not self.notifying:
@@ -318,6 +354,7 @@ class RowerData(Characteristic):
         GLib.timeout_add(200, self.Waterrower_cb)
 
     def StartNotify(self):
+        logger.debug("Entering RowerData.StartNotify")
         if self.notifying:
             print('Already notifying, nothing to do')
             return
@@ -326,6 +363,7 @@ class RowerData(Characteristic):
         self._update_Waterrower_cb_value()
 
     def StopNotify(self):
+        logger.debug("Entering RowerData.StopNotify")
         if not self.notifying:
             print('Not notifying, nothing to do')
             return
@@ -340,9 +378,11 @@ class RowerData(Characteristic):
 # first 2 bytes: are for rowing machine details: 0B
 
 class FitnessMachineControlPoint(Characteristic):
+    logger.debug("Entering Class FitnessMachineControlPoint")
     FITNESS_MACHINE_CONTROL_POINT_UUID = '2ad9'
 
     def __init__(self, bus, index, service):
+        logger.debug("Entering FitnessMachineControlPoint.init")
         Characteristic.__init__(
             self, bus, index,
             self.FITNESS_MACHINE_CONTROL_POINT_UUID,
@@ -351,6 +391,7 @@ class FitnessMachineControlPoint(Characteristic):
         self.out_q = None
 
     def fmcp_cb(self, byte):
+        logger.debug("Entering FitnessMachineControlPoint.fmcp_cb")
         print('fmcp_cb activate')
         print(byte)
         if byte == 0:
@@ -362,6 +403,7 @@ class FitnessMachineControlPoint(Characteristic):
         self.PropertiesChanged(GATT_CHRC_IFACE, {'Value': value}, [])
 
     def WriteValue(self, value, options):
+        logger.debug("Entering FitnessMachineControlPoint.WriteValue")
         self.value = value
         print(value)
         byte = self.value[0]
@@ -374,17 +416,21 @@ class FitnessMachineControlPoint(Characteristic):
             self.fmcp_cb(byte)
 
 class HeartRate(Service):
+    logger.debug("Entering Class HeartRate")
     HEART_RATE = '180D'
 
     def __init__(self, bus, index):
+        logger.debug("Entering HeartRate.init")
         Service.__init__(self, bus, index, self.HEART_RATE, True)
         self.add_characteristic(HeartRateMeasurement(bus, 0, self))
 
 class HeartRateMeasurement(Characteristic):
+    logger.debug("Entering Class HeartRateMeasurement")
     HEART_RATE_MEASUREMENT = '2a37'
     last_hr = 0
 
     def __init__(self, bus, index, service):
+        logger.debug("Entering HeartRateMeasurement.init")
         Characteristic.__init__(
             self, bus, index,
             self.HEART_RATE_MEASUREMENT,
@@ -393,6 +439,7 @@ class HeartRateMeasurement(Characteristic):
         self.notifying = False
 
     def Waterrower_cb(self):
+        logger.debug("Entering HeartRateMeasurement.Waterrower_cb")
         hr = WaterrowerValuesRaw['heart_rate'];
         if self.last_hr != hr:
             self.last_hr = hr
@@ -403,6 +450,7 @@ class HeartRateMeasurement(Characteristic):
         return self.notifying
 
     def _update_Waterrower_cb_value(self):
+        logger.debug("Entering HeartRateMeasurement.update_waterrower_cb_values")
         print('Update Waterrower HR Data')
 
         if not self.notifying:
@@ -411,6 +459,7 @@ class HeartRateMeasurement(Characteristic):
         GLib.timeout_add(1000, self.Waterrower_cb)
 
     def StartNotify(self):
+        logger.debug("Entering HeartRateMeasurement.StartNotify")
         if self.notifying:
             print('Already notifying, nothing to do')
             return
@@ -420,6 +469,7 @@ class HeartRateMeasurement(Characteristic):
         self._update_Waterrower_cb_value()
         
     def StopNotify(self):
+        logger.debug("Entering HeartRateMeasurement.StopNotify")
         if not self.notifying:
             print('Not notifying, nothing to do')
             return
@@ -428,7 +478,9 @@ class HeartRateMeasurement(Characteristic):
 
 
 class FTMPAdvertisement(Advertisement):
+    logger.debug("Entering Class FTMPAdvertisement")
     def __init__(self, bus, index):
+        logger.debug("Entering FTMPAdvertisement.init")
         Advertisement.__init__(self, bus, index, "peripheral")
         self.add_manufacturer_data(
             0xFFFF, [0x77, 0x72],
@@ -443,14 +495,17 @@ class FTMPAdvertisement(Advertisement):
 
 
 def register_ad_cb():
+    logger.debug("Entering FTMPAdvertisement.register_ad_cb")
     logger.info("Advertisement registered")
 
 
 def register_ad_error_cb(error):
+    logger.debug("Entering FTMPAdvertisement.register_ad_error_cb")
     logger.critical("Failed to register advertisement: " + str(error))
     mainloop.quit()
 
 def sigint_handler(sig, frame):
+    logger.debug("Entering FTMPAdvertisement.sigint_handler")
     if sig == signal.SIGINT:
         mainloop.quit()
     else:
@@ -461,6 +516,7 @@ AGENT_PATH = "/com/inonoob/agent"
 WaterrowerValuesRaw_polled = None
 
 def Waterrower_poll():
+    logger.debug("Entering Waterrower_poll")
     global WaterrowerValuesRaw
     global WaterrowerValuesRaw_polled
 
@@ -477,51 +533,74 @@ def Waterrower_poll():
 
 
 def main(out_q,ble_in_q): #out_q
+    logger.debug("main: Entering main")
     global mainloop
     global out_q_reset
     global ble_in_q_value
     out_q_reset = out_q
     ble_in_q_value = ble_in_q
 
+    logger.debug("main: Calling dbus.mainloop.glib.DBusGMainLoop")
     dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
 
+    logger.debug("main: Getting System bus")
     # get the system bus
     bus = dbus.SystemBus()
+
+    logger.debug("main: Getting ble controller")
     # get the ble controller
     adapter = find_adapter(bus)
 
     if not adapter:
-        logger.critical("GattManager1 interface not found")
+        logger.critical("main: GattManager1 interface not found")
         return
 
+    logger.debug("main: Getting Bluez service")
     adapter_obj = bus.get_object(BLUEZ_SERVICE_NAME, adapter)
 
+    logger.debug("main: Getting Bluez properties")
     adapter_props = dbus.Interface(adapter_obj, "org.freedesktop.DBus.Properties")
 
     # powered property on the controller to on
+    logger.debug("main: Set bluez powered property to on")
     adapter_props.Set("org.bluez.Adapter1", "Powered", dbus.Boolean(1))
 
     # Get manager objs
+    logger.debug("main: Getting Bluez GATT MANAGER IFACE")
     service_manager = dbus.Interface(adapter_obj, GATT_MANAGER_IFACE)
+    logger.debug("main: Getting Bluez ADVERTISING MANAGER IFACE")
     ad_manager = dbus.Interface(adapter_obj, LE_ADVERTISING_MANAGER_IFACE)
 
+    logger.debug("main: Calling FTMPAdvertisement")
     advertisement = FTMPAdvertisement(bus, 0)
+
+    logger.debug("main: Get bus object for bluez service")
     obj = bus.get_object(BLUEZ_SERVICE_NAME, "/org/bluez")
 
+    logger.debug("main: Get Agent")
     agent = Agent(bus, AGENT_PATH)
 
+    logger.debug("main: Set app object")
     app = Application(bus)
+    logger.debug("main: Calling add_service - DeviceInformation")
     app.add_service(DeviceInformation(bus, 1))
+    logger.debug("main: Calling add_service - FTMservice")
     app.add_service(FTMservice(bus, 2))
+    logger.debug("main: Calling add_service - HeartRate")
     app.add_service(HeartRate(bus,3))
 
+    logger.debug("main: Set up Waterrower_poll recurring task")
     GLib.timeout_add(100, Waterrower_poll)
 
+    logger.debug("main: associate Mainloop to mainloop")
     mainloop = MainLoop()
 
+    logger.debug("main: Set agent manager")
     agent_manager = dbus.Interface(obj, "org.bluez.AgentManager1")
+    logger.debug("main: Register bluetooth agent with noinputnooutput")
     agent_manager.RegisterAgent(AGENT_PATH, "NoInputNoOutput") # register the bluetooth agent with no input and output which should avoid asking for pairing 
 
+    logger.debug("main: calling admanager.RegisterAdvertisement")
     ad_manager.RegisterAdvertisement(
         advertisement.get_path(),
         {},
@@ -529,7 +608,7 @@ def main(out_q,ble_in_q): #out_q
         error_handler=register_ad_error_cb,
     )
 
-    logger.info("Registering GATT application...")
+    logger.info("main: Calling service_manager.RegisterApplication to Registering GATT application...")
 
     service_manager.RegisterApplication(
         app.get_path(),
@@ -538,8 +617,10 @@ def main(out_q,ble_in_q): #out_q
         error_handler=[register_app_error_cb],
     )
 
+    logger.debug("main: agent_manager.RequestDefaultAgent")
     agent_manager.RequestDefaultAgent(AGENT_PATH)
 
+    logger.debug("main: mainloop.run")
     mainloop.run()
     # ad_manager.UnregisterAdvertisement(advertisement)
     # dbus.service.Object.remove_from_connection(advertisement)
